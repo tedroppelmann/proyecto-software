473b2c001f882d56b914e775543d275b523bd70c
Tomás Droppelmann <tedroppelmann@uc.cl>
2020-10-05 11:57:40 -0300

    cambio esquema db

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_10_04_185942) do
-=======
 ActiveRecord::Schema.define(version: 2020_10_05_035817) do
->>>>>>> CRUD_usuario
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "comunas", force: :cascade do |t|
     t.string "nombre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_035817) do
     t.integer "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> CRUD_usuario
   end
 
 end
