f788ebc6be126b759b6ab0fc9b6714707c635a0f
Crespy98 <tasoto@uc.cl>
2020-10-08 00:36:43 -0300

    test de comuna

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_035817) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.string "first_name"
     t.string "last_name"
     t.integer "age", default: 0
--- spec/models/comuna_spec.rb
+++ spec/models/comuna_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comuna, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create correctly" do
+    comuna = Comuna.new(:nombre => "San Joaquin")
+    expect(comuna).to be_valid
+  end
+
+  context "when you try to create a comuna with out a name" do
+    it "should not create because do not have a name" do
+      comuna = Comuna.new()
+      expect(comuna).not_to be_valid
+
+    end
+
+  end
+
 end
