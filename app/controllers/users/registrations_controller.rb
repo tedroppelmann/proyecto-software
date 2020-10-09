5aea85d30dc5e44284ac384314c76b505bcddc38
Tomás Droppelmann <tedroppelmann@uc.cl>
2020-10-09 20:48:38 -0300

    arreglo user editar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [:create]
     before_action :authenticate_user!
-    # before_action :configure_account_update_params, only: [:update]
+    before_action :configure_account_update_params, only: [:update]
 
     # GET /resource/sign_up
     # def new
@@ -48,9 +48,9 @@ module Users
     end
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_account_update_params
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-    # end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :age, :phone_number])
+    end
 
     # The path used after sign up.
     # def after_sign_up_path_for(resource)
