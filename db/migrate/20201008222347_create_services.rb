a8ff0fdacae6a974c4900cfa69afd4e3556408ae
tomishishigang <tadubost@uc.cl>
2020-10-08 21:56:51 -0300

    CRUD servicio listoco

---
+++ db/migrate/20201008222347_create_services.rb
@@ -0,0 +1,12 @@
+class CreateServices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :services do |t|
+      t.string :nombre
+      t.string :descripcion
+      t.integer :cap_max
+      t.integer :precio
+
+      t.timestamps
+    end
+  end
+end
