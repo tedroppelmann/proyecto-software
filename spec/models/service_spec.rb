a7a180181b1ff9fe6a691ec53909071c443b3ad4
Tomás Droppelmann <tedroppelmann@uc.cl>
2020-10-09 11:52:02 -0300

    rucopob arreglado_3

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -3,28 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  it 'should create correctly' do
+  it 'creates correctly' do
     service = Service.new(nombre: 'los limpia pisos', descripcion: 'te voy a limpiar el piso al final del carrete', cap_max: 3, precio: 500)
     expect(service).to be_valid
   end
 
   context 'when you try to create a service with out a name' do
-    it 'should not create because do not have a name' do
+    it 'does not create because do not have a name' do
       service = Service.new(descripcion: 'te voy a limpiar el piso al final del carrete', cap_max: 3, precio: 500)
       expect(service).not_to be_valid
     end
 
-    it 'should not create because do not have a description' do
+    it 'does not create because do not have a description' do
       service = Service.new(nombre: 'los limpia pisos', cap_max: 3, precio: 500)
       expect(service).not_to be_valid
     end
 
-    it 'should not create because do not have a cap_max' do
+    it 'does not create because do not have a cap_max' do
       service = Service.new(nombre: 'los limpia pisos', descripcion: 'te voy a limpiar el piso al final del carrete', precio: 500)
       expect(service).not_to be_valid
     end
 
-    it 'should not create because do not have a precio' do
+    it 'does not create because do not have a precio' do
       service = Service.new(nombre: 'los limpia pisos', descripcion: 'te voy a limpiar el piso al final del carrete', cap_max: 3)
       expect(service).not_to be_valid
     end
--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -4,11 +4,12 @@ require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
   describe '/home' do
-    it 'should return a succesful request' do
+    it 'returns a succesful request' do
       get '/static_pages/home'
       expect(response).to have_http_status(:ok)
     end
-    it 'should render the home view' do
+
+    it 'renders the home view' do
       get '/static_pages/home'
       expect(response).to render_template(:home)
     end
