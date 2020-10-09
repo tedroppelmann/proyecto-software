37c0b07ae6ba9a4fbdad7f805c86bdac46acbad4
Tomás Droppelmann <tedroppelmann@uc.cl>
2020-10-09 09:43:27 -0300

    arreglo rubocop

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,6 @@
 class Service < ApplicationRecord
     validates :nombre, :presence => true
     validates :descripcion, :presence => true
-    validates :cap_max, :presence => true , :numericality => {:only_interger => true}
-    validates :precio, :presence => true , :numericality => {:only_interger => true}
+    validates :cap_max, :presence => true , :numericality => { :only_interger => true }
+    validates :precio, :presence => true , :numericality => { :only_interger => true }
 end
