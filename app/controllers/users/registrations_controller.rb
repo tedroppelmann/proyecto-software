b921e23488cfa12f7c53d9d17ce198ffff3e6798
Tomás Droppelmann <tedroppelmann@uc.cl>
2020-10-09 21:15:38 -0300

    cambio rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,7 +49,7 @@ module Users
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :age, :phone_number])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name age phone_number])
     end
 
     # The path used after sign up.
