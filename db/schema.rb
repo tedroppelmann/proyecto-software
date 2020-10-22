f4b85f8254c78b0cb95e43e1167d257678d2fc58
Crespy98 <tasoto@uc.cl>
2020-10-22 00:01:29 -0300

    cambio en la forma de mostar la fecha en los comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_015507) do
+ActiveRecord::Schema.define(version: 2020_10_21_144555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comentarios", force: :cascade do |t|
     t.text "contenido"
-    t.string "fecha"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
