a9ca8849a5667e0877173e5e0b2e34eb1eef5298
Crespy98 <tasoto@uc.cl>
2020-10-29 02:35:47 -0300

    migracion de comentario a servicio

--- db/migrate/20201029031514_add_comuna_name_to_parties.rb
+++
@@ -1,5 +0,0 @@
-class AddComunaNameToParties < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :parties, :comuna, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_031514) do
+ActiveRecord::Schema.define(version: 2020_10_29_052756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_031514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "service_id"
+    t.index ["service_id"], name: "index_comentarios_on_service_id"
     t.index ["user_id"], name: "index_comentarios_on_user_id"
   end
 
@@ -69,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_031514) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comentarios", "services"
   add_foreign_key "comentarios", "users"
   add_foreign_key "parties", "comunas"
   add_foreign_key "parties", "users"
