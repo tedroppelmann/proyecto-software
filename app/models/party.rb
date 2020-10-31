341c196d2fbeed059da13353663132564b56237e
Crespy98 <tasoto@uc.cl>
2020-10-30 23:28:30 -0300

    arreglo eliminar carrete

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,10 +3,10 @@
 class Party < ApplicationRecord
     belongs_to :user
     belongs_to :comuna
-    has_and_belongs_to_many :services
+    has_and_belongs_to_many :services, dependent: :destroy
     
 
     #para ver lo de las apuestas
-    has_many :assistants
+    has_many :assistants, dependent: :destroy
     has_many :users, through: :assistants
 end
