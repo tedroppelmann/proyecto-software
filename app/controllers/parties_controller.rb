702e0cd31d2eee73b8eeea9408be077f381cf14f
Tomás Droppelmann <tedroppelmann@uc.cl>
2020-11-19 22:21:12 -0300

    areglos comunas

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -16,7 +16,7 @@ class PartiesController < ApplicationController
   end
 
   def create
-    @parties_param = params.require(:party).permit(:titulo, :descripcion, :fecha, :direccion, :capacidad, :costo, :user_id, :comuna_id, { service_ids: [] }, :code)
+    @parties_param = params.require(:party).permit(:titulo, :descripcion, :fecha, :direccion, :capacidad, :costo, :user_id, :comuna_id, :code, { service_ids: [] })
     @party = Party.create(@parties_param)
 
     if @party.save
@@ -53,7 +53,7 @@ class PartiesController < ApplicationController
 
   def update
     @party = Party.find(params[:id])
-    @parties_param = params.require(:party).permit(:titulo, :descripcion,:fecha, :direccion, :capacidad, :costo, { service_ids: [] })
+    @parties_param = params.require(:party).permit(:titulo, :descripcion, :fecha, :direccion, :capacidad, :costo, :user_id, :comuna_id, :code, { service_ids: [] })
 
     if @party.update(@parties_param)
       redirect_to parties_index_path
