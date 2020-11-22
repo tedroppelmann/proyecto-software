86c978a1f9ec81cda29658d91f71fdf47b87d7f6
tomishishigang <tadubost@uc.cl>
2020-11-22 04:11:28 -0300

    listos los test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # para ver lo de las apuestas
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :admin, presence: false
+  validates :monedero, presence: false
+  validates :image, presence: true
   has_one_attached :image
   has_many :assistants, dependent: :destroy
   has_many :partys, through: :assistants
--- spec/requests/comunas_request_spec.rb
+++ spec/requests/comunas_request_spec.rb
@@ -3,37 +3,56 @@
 require 'rails_helper'
 
 RSpec.describe 'Comunas', type: :request do
-  let(:comuna) do
-    comuna = Comuna.new(nombre: 'San Joaquin')
-  end
-
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/comunas/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/comunas/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  # describe "GET /show" do
-  # it "returns http success" do
-  #  comuna = Comuna.find(params[:id])
-  #  get "/comunas/show"
-  #  expect(response).to have_http_status(:success)
+  # before do 
+  #   @user = User.create(first_name: 'Tomardo', last_name: 'Dubost', email: 'tomas@tomas.com', age: 21, phone_number: 952975, admin: true, monedero: 0, image: nil)
+  #   puts(@user)
+  #   puts('--------------------')
+  #   puts(@user.id)
+  #   puts('--------------------a')
+  #   sign_in @user
   # end
+
+  # describe 'GET /index' do
+  #   it 'returns http success' do
+  #     get '/comunas/index'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  #   it 'show a comuna' do 
+  #     comuna = Comuna.create(nombre: 'Mypooh')
+  #     get comuna_path(comuna.id)
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  # # after do
+  # #   sign_out @user
+  # # end
+  # describe 'GET /new' do
+  #   it 'returns http success' do
+  #     get '/comunas/new'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  # # describe 'POST /create' do
+  # #   comuna = Comuna.create(nombre: 'Santiago')
+  # #   it 'creates a new comuna' do
+  # #     post comuna_path, params: comuna.id
+  # #     expect(response).to redirect_to comuna_new_path
+  # #   end
+  # # end
+    
+  # describe "GET /show" do
+  #   @comuna = Comuna.create(nombre: 'Mypooh')
+  #   it "returns http success" do
+  #     get "/comunas/show"
+  #     expect(response).to render_template('comunas/show')
+  #   end
   # end
 
   # describe "GET /edit" do
-  # it "returns http success" do
-  #  comuna = Comuna.find(params[:id])
-  #  get "/comunas/edit"
-  #  expect(response).to have_http_status(:success)
-  # end
+  #   it "returns http success" do
+  #   comuna = Comuna.find(params[:id])
+  #   get "/comunas/edit"
+  #   expect(response).to have_http_status(:success)
+  #   end
   # end
 end
